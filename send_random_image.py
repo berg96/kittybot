1158ca4e15406b8b72ea985d2d6207c01eacd465
Artem Kulikov <chigar2010@yandex.ru>
2023-11-11 23:12:42 +0200

    upd

--- send_random_image.py
+++ send_random_image.py
@@ -1,12 +1,30 @@
+import requests
+
 from telegram import Bot
 
 bot = Bot(token='6343280383:AAFNKMEitLvnscYQkiO_aSHwyLlGEt2mnUo')
+# Адрес API сохраним в константе
+URL = 'https://api.thecatapi.com/v1/images/search'
+# Сделаем GET-запрос к API
+# метод json() преобразует полученный ответ JSON в тип данных, понятный Python
+response = requests.get(URL).json()
+# Рассмотрим структуру и содержимое переменной response
+print(response)
 
-URL = 'https://cdn2.thecatapi.com/images/3dl.jpg'
+# Посмотрим, какого типа переменная response
+print(type(response))
+
+# response - это список. А какой длины?
+print(len(response))
+
+# Посмотрим, какого типа первый элемент
+print(type(response[0]))
+
+random_cat_url = response[0].get('url')
 
 chat_id = 415404984
 text = 'Вам телеграмма!'
 # Отправка сообщения
 bot.send_message(chat_id, text)
 # Отправка изображения
-bot.send_photo(chat_id, URL)
+bot.send_photo(chat_id, random_cat_url)
